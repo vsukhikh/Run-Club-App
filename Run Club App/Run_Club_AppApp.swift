27807b6b54819540d80173afbbe70473842c511b
Vladimir Sukhikh <vsukhikh@icloud.com>
2022-08-25 00:35:37 -0700

    Initial Commit

---
+++ Run Club App/Run_Club_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Run_Club_AppApp.swift
+//  Run Club App
+//
+//  Created by Vladimir Sukhikh on 2022-08-25.
+//
+
+import SwiftUI
+
+@main
+struct Run_Club_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
